f5724d025a69a798074b88f48795d6cbaf5668c0
Mathan Kumar <104348658+mathankumar98@users.noreply.github.com>
2022-10-13 18:17:36 +0530

    JavaScript Functions Date-13.10.2022

---
+++ Functions.js
@@ -0,0 +1,33 @@
+//Functions
+
+//Sample Function Creation And Calling the function
+function method1()
+{
+    let a="Hi Iam Mathan From Variable a";
+    console.log(a);
+}
+method1();
+
+//Parametazied Function
+//In This parametazied Funtion We Can Able to initialize some value in this function
+function method2(name)
+{
+    let b="Hi Iam "+name+" From Variable b";
+    console.log(b);
+}
+method2("Mathan Kumar");
+
+//Double value loading parametarized function
+function method3(Firstname,lastname)
+{
+    let c="Hi Iam "+Firstname+lastname+" From variable c";
+    console.log(c);
+}
+method3("ar","rahman");
+
+//adding 2 numbers using parametazied function
+function method4(Number1,Number2)
+{
+    console.log(Number1+Number2);
+}
+method4(50,50);
