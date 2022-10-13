00b2965a8246487bd621f8673ddbefef8da7bbaf
Mathan Kumar <104348658+mathankumar98@users.noreply.github.com>
2022-10-13 15:38:15 +0530

    Cloning the object Date-13.10.2022

---
+++ Cloning.js
@@ -0,0 +1,12 @@
+//cloning is used to copy and create duplicate to the object is called cloning
+// we use spread operator to achieve the cloning
+let user={
+    name:'Mathan',
+    getfullname(user1)
+    {
+        user1.name='Mathan Kumar';
+        console.log(user1);
+    }
+}
+let another={...user}//spread operator
+console.log(another);
