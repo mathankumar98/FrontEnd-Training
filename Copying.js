328afbb0f1d0505ab0d7d4f5090ff557522b78b0
Mathan Kumar <104348658+mathankumar98@users.noreply.github.com>
2022-10-13 15:39:16 +0530

    Copying The Object Date-13.10.2022

---
+++ Copying.js
@@ -0,0 +1,12 @@
+//By Using Assign KeyWord to Coping the object
+var a={
+    name1:"Mathan",
+    age:24
+}
+//We Can do Copying by this type also
+//let b={name2:"Kumar"};
+//Object.assign(a,b);
+
+//we can do copying by this type also
+Object.assign(a,{name2:"Kumar"});
+console.log(a);
