69ca3f218b050bf11fefd9d11f87add8c66d910d
Mathan Kumar <104348658+mathankumar98@users.noreply.github.com>
2022-10-13 15:33:55 +0530

    Comparision Operator Date-13.10.2022

---
+++ Comparision.opt.js
@@ -0,0 +1,38 @@
+//Comparision Operator
+//Comparision only Accept The Binary operator need both side values
+//1. ==
+//2. ===
+//3. !=
+//4. !==
+//5. >
+//6. >=
+//7. <
+//8. <=
+var a=10;
+var b=20;
+var c=a;
+if(a==c)
+{
+    console.log("Both Numbers Are Same");
+}
+else
+{
+    console.log("Both Numbers Are Not Same");
+}
+if(b<=a)
+{
+    console.log("Hi From if Condition");
+}
+else
+{
+    console.log("Bye From Else Condition");
+
+}
+if(b!=a)
+{
+    console.log("Yes b Is Not Equal To a");
+}
+else
+{
+    console.log("Yes B Is Equal To a");
+}
