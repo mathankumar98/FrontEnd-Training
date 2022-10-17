f2c6fcdba02d14e1230569b978a5f0c785f1b574
Mathan Kumar <104348658+mathankumar98@users.noreply.github.com>
2022-10-17 12:08:25 +0530

    JavaScript ArrayMethods Date-17.10.2022

---
+++ Array Methods.js
@@ -0,0 +1,27 @@
+//Array Methods
+//In Array Methods We Have Few Principles To Achieve Methods
+//1.push-Adding The Element
+//2.pop-Removing The Element
+//3.concat-Merging The 2 Array Variables
+//4.join-Joining The Array Elements
+//5.reverse-Reversing The Array Element
+
+var a=["a","b","c","d"];//Variable Declaration And Initialization
+var b=[1,2];
+
+//1.Push Method()-Adding The Extra Element In a Array
+console.log(a.push("e"));
+console.log(a);
+
+//2.pop Method()-Removing The Element In the Array
+console.log(a.pop());
+console.log(a);
+
+//3.concat Method()-Merging Two Array Variables
+console.log(a.concat(b));
+
+//4.join Method()-join The Array Elements With Any Single courts
+console.log(a.join("*"));
+
+//5.reverse Method()-Reversing The Array Element
+console.log(a.reverse());
