a05637fb7a1cc51b798b06b363cddb6d25bcb817
Mathan Kumar <104348658+mathankumar98@users.noreply.github.com>
2022-10-18 18:04:31 +0530

    Javascript Prototypal Inhertance Date-18.10.2022

---
+++ Prototypal inheritance.js
@@ -0,0 +1,17 @@
+//Prototypal Inhertance
+//In The Prototypal Inhertance We Can Able To call with the help of object creation and using .
+//using dot . we can able to acheive the prototypal inhertance
+const i=
+{
+    a:1,
+    b:2,
+    proto:
+    {
+        b:3,
+        d:4,
+    }
+}
+console.log(i.a)
+console.log(i.b)
+console.log(i.proto.b)
+console.log(i.proto.d)
